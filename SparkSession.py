19e4c10aa653e68e89456f847c7b00c18e1489e7
mihir_singh <52312635+mihir-singh@users.noreply.github.com>
2019-06-28 13:42:36 +0530

    Update SparkSession.py

--- SparkSession.py
+++ SparkSession.py
@@ -1,6 +1,14 @@
+#import a SparkSession
 from pyspark.sql import SparkSession
+#create a sparkSession
 spark=SparkSession.builder.appName("grouping").getOrCreate()
+#read the file with header option true
 df=spark.read.csv("custormer-orders.csv",inferSchema=True,header=True)
+#see the results
 df.show()
+#group the data
 grouped=df.groupBy("dept").count().orderBy("count",ascending=False)
+#show grouped data
 grouped.show()
+#another way to show distinct data in descending order
+df.select("genres").distinct().orderBy("genres",ascending=False).show()
